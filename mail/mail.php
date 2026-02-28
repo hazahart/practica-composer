2eca7a76a60d73bf2988af5c3fc5e71a4cf74cde
Gustavo Ramirez Mireles <gusramireles@hotmail.com>
2026-02-27 22:53:46 -0600

    Correccion de puerto y encriptación. Se agregaron bloques try-catch

--- mail/mail.php
+++ mail/mail.php
@@ -1,29 +1,36 @@
 <?php
-
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-class Mail {
-    public function sendMail($destinatario, $nombreDestinatario) {
-        require dirname(__DIR__) ."/vendor/autoload.php";
-        $mail = new PHPMailer();
-        $mail->isSMTP();
-        $mail->Host = "smtp.gmail.com";
-        $mail->Port = 465;
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-        $mail->SMTPAuth = true;
-        $mail->Username = $_ENV['SENDER'];
-        $mail->Password = $_ENV['SENDER_PASSWORD'];
-        $mail->setFrom($_ENV['SENDER'], $_ENV['SENDER_NAME']);
-        $mail->addAddress($destinatario, $nombreDestinatario);
-        $mail->Subject = "Contacto";
-        $mail->CharSet = PHPMailer::CHARSET_UTF8;
-        $mail->isHTML(true);
-        ob_start();
-        require dirname(__DIR__) . '/views/message.php';
-        $mail->Body = ob_get_clean();
-        if ($mail->send()) {
+class Mail
+{
+    public function sendMail($destinatario, $nombreDestinatario)
+    {
+        try {
+            require dirname(__DIR__) ."/vendor/autoload.php";
+            $mail = new PHPMailer();
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = $_ENV['SENDER'];
+            $mail->Password = $_ENV['SENDER_PASSWORD'];
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->Port = 587;
+            $mail->setFrom($_ENV['SENDER'], $_ENV['SENDER_NAME']);
+            $mail->addAddress($destinatario, $nombreDestinatario);
+            $mail->isHTML(true);
+            $mail->Subject = "¡Bienvenido a nuestro Boletín!";
+            $mail->CharSet = PHPMailer::CHARSET_UTF8;
+
+            ob_start();
+            require dirname(__DIR__) . '/views/message.php';
+            $mail->Body = ob_get_clean();
+
+            $mail->send();
             return true;
-        } else {
+
+        } catch (Exception $e) {
+            error_log("Error al enviar correo: {$mail->ErrorInfo}");
             return false;
         }
     }
